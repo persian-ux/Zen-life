a9880e9cf3e8905cac713488bc8a344eb9c42d4f
persian-ux <rb5212978@gmail.com>
2026-01-12 08:36:43 +0500

    delete unneccessary files

--- components/auth/index.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardComponent from './Dashboard';
-import LoginComponent from './Login';
-
-export { default as Dashboard } from './Dashboard';
-export { default as Login } from './Login';
-
-// Default export as an object of the actual components for convenience
-const DefaultExport = { Login: LoginComponent, Dashboard: DashboardComponent };
-export default DefaultExport;
